a26e90305b95359430ac480cbeec7a5636a1522f
Cande <almeida_bazaldua@alumnos.cei.es>
2025-03-20 17:03:36 +0100

    vercel

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# vercel-backend-demo
+
+
+
+# Vercel backend 
+
+Pasos a seguir:
+
+1. Configuramos backend express
+        1. npm init 
+        2. npm install express
+        3. npm i nodemon -D 
+        4. dotenv, cors, mongoose
+        5. type module 
+        6. scripts
+   
+2. Contenido estático (carpeta public)
+
---
+++ vercel-backend/backend/index.js
@@ -0,0 +1,68 @@
+
+import express from 'express';
+import  Router  from 'express';
+
+//------ config.js ----------
+import dotenv from 'dotenv';
+dotenv.config(); //iniciamos lectura de variables
+
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || 'http://localhost'
+const NOMBRE = process.env.NOMBRE || 'Mundo'
+//--------------------------
+
+const app = express()
+
+//Middlewares
+app.use(express.json()) //Parse procesa el json body para leer con req body
+app.use(express.urlencoded({extended: false})) //leer datos de url encoded con req.body
+
+
+
+
+
+//contenido estático 
+//así entro a mi carpeta public uploads
+app.use("/uploads", express.static('public/uploads')) 
+app.use("/web" , express.static('public'))
+
+
+
+
+
+
+
+app.get("/", (req, res, next) => {
+    res.setHeader("Content-Type", "text/html")   //respueta en html 
+
+
+    const landingHTML = `
+    
+        <h1>  Hola ${NOMBRE} </h1>
+        Bienvenido a nuestro backend en Express :))
+    
+    `;
+    res.send(landingHTML)
+})
+
+
+
+//rutas de mi API
+//(routes/index.routes.js)
+const router = Router();
+
+router.get("/", (req, res, next) => {
+    res.json({message:"Bienvenidos a nuestra API v1 :))"})
+})
+
+router.get("/users", (req, res, next ) => {
+    res.json({message: "Rutas para obtener usuarios :))"})
+})
+
+
+app.use('/api/v1', router)  //se usa esta url porque router está dentro 
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en ${HOST}:${PORT}`)
+})
